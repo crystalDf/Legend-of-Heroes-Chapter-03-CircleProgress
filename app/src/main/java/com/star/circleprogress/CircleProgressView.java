19492f676af57c132ff274a195a0f0afeba321b9
crystalDf <chendong333@gmail.com>
2017-02-02 23:27:21 +0800

    Chapter 03 CircleProgress

--- app/src/main/java/com/star/circleprogress/CircleProgressView.java
+++ app/src/main/java/com/star/circleprogress/CircleProgressView.java
@@ -2,11 +2,16 @@ package com.star.circleprogress;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 
 public class CircleProgressView extends View {
+
+    private Paint mCirclePaint;
+    private float mCircleXY;
+    private float mRadius;
 
     public CircleProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
