963d242f0927b14ef0f428b02e429747ea118c9c
crystalDf <chendong333@gmail.com>
2017-02-13 22:22:35 +0800

    Chapter 03 CircleProgress

--- app/src/main/java/com/star/circleprogress/CircleProgressView.java
+++ app/src/main/java/com/star/circleprogress/CircleProgressView.java
@@ -3,6 +3,8 @@ package com.star.circleprogress;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,9 +14,18 @@ public class CircleProgressView extends View {
     private int mMeasureWidth;
     private int mMeasureHeight;
 
-    private Paint mCirclePaint;
     private float mCircleXY;
     private float mRadius;
+    private Paint mCirclePaint;
+
+    private RectF mArcRectF;
+
+    private float mSweepAngle;
+    private Paint mArcPaint;
+
+    private String mText;
+    private int mTextSize;
+    private Paint mTextPaint;
 
     public CircleProgressView(Context context) {
         super(context);
@@ -32,11 +43,19 @@ public class CircleProgressView extends View {
 
         setMeasuredDimension(mMeasureWidth, mMeasureHeight);
 
+        init();
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        canvas.drawCircle(mCircleXY, mCircleXY, mRadius, mCirclePaint);
+        canvas.drawArc(mArcRectF, 0, mSweepAngle, false, mArcPaint);
+        canvas.drawText(mText, 0, mText.length(), mCircleXY, mCircleXY + mTextSize / 4, mTextPaint);
+
+        invalidate();
     }
 
     private void init() {
@@ -47,7 +66,35 @@ public class CircleProgressView extends View {
         mRadius = (float) (length * 0.5 / 2);
 
         mCirclePaint = new Paint();
+        mCirclePaint.setAntiAlias(true);
+        mCirclePaint.setColor(ContextCompat.getColor(getContext(),
+                android.R.color.holo_blue_bright));
 
+        float arcStart = (float) (length * 0.1);
+        float arcEnd = (float) (length * 0.9);
+
+        mArcRectF = new RectF(arcStart, arcStart, arcEnd, arcEnd);
+
+        mArcPaint = new Paint();
+        mArcPaint.setAntiAlias(true);
+        mArcPaint.setColor(ContextCompat.getColor(getContext(), android.R.color.holo_blue_bright));
+        mArcPaint.setStyle(Paint.Style.STROKE);
+        mArcPaint.setStrokeWidth(arcStart);
+
+        mText = "Android";
+        mTextSize = 50;
+
+        mTextPaint = new Paint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setTextSize(mTextSize);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+
+    }
+
+    public void setSweepAngle(float sweepAngle) {
+        mSweepAngle = (sweepAngle == 0 ? 90 : sweepAngle);
+
+        invalidate();
     }
 
 }
