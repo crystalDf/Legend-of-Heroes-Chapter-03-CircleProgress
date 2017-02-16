2f7b3b7e31cc628647b3044a08b31e74c4f892bb
crystalDf <chendong333@gmail.com>
2017-02-16 23:04:28 +0800

    Chapter 03 CircleProgress

--- app/src/main/java/com/star/circleprogress/MainActivity.java
+++ app/src/main/java/com/star/circleprogress/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CircleProgressView mCircleProgressView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCircleProgressView = (CircleProgressView) findViewById(R.id.circle_progress);
     }
 }
